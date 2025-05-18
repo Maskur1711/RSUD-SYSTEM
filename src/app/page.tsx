9b2b7d11697b9375b07de2aa4af89eb162a4fe27
Maskur1711 <115292418+Maskur1711@users.noreply.github.com>
2025-05-18 16:44:27 +0700

    update page awal animatied Hospital

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,23 @@
+"use client";
+
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center gap-6 p-4 text-center">
-      <h1 className="text-4xl font-bold">Welcome to My App</h1>
+      <h1 className="text-4xl font-bold">
+        SELAMAT DATANG DI APLIKASI RSUD HOLIS
+      </h1>
+      <DotLottieReact
+        src="https://lottie.host/2eeb033d-f79d-48fd-9133-53efdfd63ffb/sLtyGcyThQ.lottie"
+        loop
+        autoplay
+      />
       <p className="max-w-md text-muted-foreground">
-        A Next.js application with sidebar navigation, authentication, and more.
+        Aplikasi ini adalah aplikasi untuk mempermudah dalam pengelolaan data di
+        RSUD Holis. Silahkan login / register untuk melanjutkan ke aplikasi.
       </p>
       <div className="flex gap-4">
         <Button asChild>
